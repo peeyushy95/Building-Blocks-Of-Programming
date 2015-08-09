7733ded791dee2c886a01303b6310c0f2dea45c8
Peeyush Yadav <peeyushy95@gmail.com>
2015-08-09 19:47:13 +0530

    Sorting

---
+++ quicksort.cpp
@@ -0,0 +1,96 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Quick Sort
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+	#ifdef Megamind
+			#define DEBUG
+			#define TRACE
+			freopen("inp.txt","r",stdin);
+			//freopen("out1.txt","w",stdout);
+	#endif
+}
+#ifdef TRACE
+	#define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+	#define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+	#define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+	#define trace(x)
+	#define trace2(x,y)
+	#define trace3(x,y,z)
+#endif
+ 
+inline ll power(ll a, ll b, ll m) {
+	ll r = 1;
+	while(b) {
+		if(b & 1) r = r * a % m;
+		a = (a * a)% m;
+		b >>= 1;
+	}
+	return r;
+}
+inline ll power(ll a, ll b) {
+	 ll r = 1;
+	while(b) {
+		if(b & 1) r = r * a ;
+		a = a * a;
+		b >>= 1;
+	}
+	return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
+#define sz 1000000
+
+int a[sz];
+
+void quicksrt(int f , int l){
+	int pivot = a[f],i,j;
+
+	if(f<l){
+		i = f;
+		j = l;
+		while(i<j){
+			
+			while(i <= l  &&  a[i] <= pivot)  i++;
+			while(j >= f  &&  a[j] >  pivot)   j--;
+			
+			if(i<j) swap(a[i],a[j]);
+					
+		}
+		swap(a[f],a[j]);
+		
+		quicksrt(f,j-1);
+		quicksrt(j+1,l);
+	}
+}
+
+int main(){
+
+	input();
+	int n;
+	s(n);
+	
+	f(i,0,n) s(a[i])
+		
+	quicksrt(0, n-1);
+	
+	p1d(a,n);
+	
+	#ifdef Megamind
+		cout << "\nTime elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
+}

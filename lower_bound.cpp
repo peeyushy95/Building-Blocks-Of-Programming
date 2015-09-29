0d4f6e8fc3ff7de21d1724adad44905dc755c1db
Peeyush Yadav <peeyushy95@gmail.com>
2015-09-29 23:25:13 +0530

    lower_bound(binary search)

---
+++ lower_bound.cpp
@@ -0,0 +1,70 @@
+/*
+   AUTHOR  : Peeyush Yadav
+   Problem : Binary Search(Lower Bound)
+*/
+#include<bits/stdc++.h>
+using namespace std; typedef long long ll; typedef pair<int,int> grp;
+inline ll power(ll a,ll b)     { ll r=1; while(b){ if(b&1) r=r*a    ; a=a*a    ; b>>=1;} return r;}
+inline ll power(ll a,ll b,ll m){ ll r=1; while(b){ if(b&1) r=(r*a)%m; a=(a*a)%m; b>>=1;} return r;}
+void fast(){	
+	#ifdef Megamind
+		freopen("inp.txt","r",stdin);
+		//freopen("out.txt","w",stdout);
+		#define trace(x)            cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+		#define trace2(x,y)         cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+		#define trace3(x,y,z)       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+	#else
+		#define trace(x)
+		#define trace2(x,y)
+		#define trace3(x,y,z)
+	#endif
+	
+	#define fi						first
+	#define se						second
+	#define mp						make_pair
+	#define pb(x)					push_back(x)
+	#define s(x)					scanf("%d",&x);
+	#define sl(x)					scanf("%lld",&x);
+	#define p(x)					printf("%d\n",x);
+	#define f(a,b,c)				for(int a=b;a<c;a++)
+	#define r(a,b,c)				for(int a=b;a>c;a--)
+	#define p2(x,y)					printf("%d %d\n",x,y);
+	#define pl(x)					printf("%lld\n",x);
+	#define pl2(x,y)				printf("%lld %lld\n",x,y);
+	#define p1d(a,n)				for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+	#define p2d(a,n,m)				for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+}
+/*........................................................END OF TEMPLATES.......................................................................*/
+#define sz 112345
+int a[sz];
+
+int lowerbound(int a[],int strt, int end , int value){
+	int low = strt , high = end - 1;
+	
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(a[mid] >= value) high = mid - 1;
+		else 				low  = mid + 1;
+	}
+	
+	return low;
+}
+int main(){
+	fast();
+	
+	int n,val;
+	s(n)
+	
+	f(i,0,n) s(a[i])
+	sort(a,a+n);
+	
+	s(val)
+	p(lower_bound(a,a+n,val) - a)
+	p(lowerbound(a,0,n,val));
+	
+	
+	
+	#ifdef Megamind
+		cerr << "\nTime elapsed : " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+	#endif
+}  
